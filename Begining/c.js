7f054dc58a925d7503cf30518739971fa63e573d
Anatoly Shandov <anatolyshandov@gmail.com>
2020-03-09 12:16:20 +0300

    mocha

--- Begining/c.js
+++ Begining/c.js
@@ -1,7 +1,6 @@
-
 describe("pow". function() {
 
-   it("Power the value to the specified degree" , function(){
+   it("Power the value to the specified degree", function(){
       assert.equal(pow(3,2), 9);   
    });
 });
